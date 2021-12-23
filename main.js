d52813f8ab9727d7334a1bbd5d377163fad37327
Jonathan Ceja <jonathan.ceja@agavelab.com>
2021-12-22 18:45:09 -0600

    Refactor variables

--- main.js
+++ main.js
@@ -2,16 +2,22 @@
     let timerNado = 0;
     let toggleNado = false;
     let intervalNado;
-
     let timerEscala = 0;
     let toggleEscala = false;
     let intervalEscala;
-
     let timerFlotacion = 0;
     let toggleFlotacion = false;
     let intervalFlotacion;
-
     let results = [];
+    let buttonEscala = document.getElementById("timer-escala-button");
+    let resetEscala = document.getElementById("timer-escala-reset");
+    let buttonFlotacion = document.getElementById("timer-flotacion-button");
+    let resetFlotacion = document.getElementById("timer-flotacion-reset");
+    let agregarElement = document.getElementById("agregar");
+    let exportElement = document.getElementById("export");
+    let clearTableElement = document.getElementById("clearTable");
+    let buttonNado = document.getElementById("timer-nado-button");
+    let resetNado = document.getElementById("timer-nado-reset");
 
     let callbackNado = () => {
         clearTimers('nado');
@@ -78,34 +84,27 @@
         document.getElementById('tableBody').innerHTML = tableContent;
     }
 
-    let buttonNado = document.getElementById("timer-nado-button");
     buttonNado.addEventListener('click', callbackNado)
 
-    let resetNado = document.getElementById("timer-nado-reset");
     resetNado.addEventListener('click', function () {
         timerNado = 0;
         updateTimerLabel('timer-nado', 0);
     })
 
-    let buttonEscala = document.getElementById("timer-escala-button");
     buttonEscala.addEventListener('click', callbackEscala)
 
-    let resetEscala = document.getElementById("timer-escala-reset");
     resetEscala.addEventListener('click', function () {
         timerEscala = 0;
         updateTimerLabel('timer-escala', 0);
     })
 
-    let buttonFlotacion = document.getElementById("timer-flotacion-button");
     buttonFlotacion.addEventListener('click', callbackFlotacion)
 
-    let resetFlotacion = document.getElementById("timer-flotacion-reset");
     resetFlotacion.addEventListener('click', function () {
         timerFlotacion = 0;
         updateTimerLabel('timer-flotacion', 0);
     })
 
-    let agregarElement = document.getElementById("agregar");
     agregarElement.addEventListener('click', function () {
         const result = [
             document.getElementById('input').value,
@@ -124,7 +123,6 @@
         updateTable();
     })
 
-    let exportElement = document.getElementById("export");
     exportElement.addEventListener('click', function () {
         const rows = [
             ["Nombre", "Tiempo de nado", "Tiempo de escala", "Tiempo de flotación"],
@@ -140,7 +138,6 @@
         window.open(encodedUri);
     })
 
-    let clearTableElement = document.getElementById("clearTable");
     clearTableElement.addEventListener('click', function () {
         results = [];
         document.getElementById('tableBody').innerHTML = "";
